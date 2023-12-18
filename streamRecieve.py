e180ad39faa37734cdcf5f26cd3cced14ac23474
Stellaris <me@stellaris.com.co.punjabifree>
2023-12-18 02:52:01 -0500

    clientside optimization

--- streamRecieve.py
+++ streamRecieve.py
@@ -9,6 +9,7 @@ from Consts import *
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, PACK_SIZE)
+client_socket.settimeout(1)
 
 host_ip = "172.17.17.120"
 port = 9999
@@ -18,9 +19,11 @@ client_socket.sendto(b'Hello',server_address)
 
 image_data = {}
 
-while cv2.waitKey(1) & 0xFF != ord('q'):
+while True:
 
     packet = client_socket.recvfrom(PACK_SIZE)[0]
+    client_socket.sendto(b'STARTING FRAME', server_address)
+
     code, id = unpack(">BH", packet[:3])
     data = packet[3:]
 
@@ -28,13 +31,13 @@ while cv2.waitKey(1) & 0xFF != ord('q'):
         print("Server connection terminated, killing client...")
         break
 
-    client_socket.sendto(b'RECIEVED', server_address)    
     frame_complete = False
 
     if len(image_data) == 0 and code != Code.FRAME_START.value and code != Code.FRAME_SOLO.value:
         continue
     
     if code == Code.FRAME_START.value or code == Code.FRAME_SOLO.value:
+
         image_data = {}
 
     image_data[id] = data
@@ -58,5 +61,8 @@ while cv2.waitKey(1) & 0xFF != ord('q'):
 
     image = cv2.imdecode(encoded_image, 1)
     cv2.imshow("RECEIVING VIDEO", image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     
 client_socket.close()
