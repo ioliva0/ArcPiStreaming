11454529e80afa679083f897ed214c22b3bce1c7
Stellaris <me@stellaris.com.co.punjabifree>
2023-12-18 01:48:11 -0500

    more testing

--- streamRecieve.py
+++ streamRecieve.py
@@ -18,15 +18,17 @@ client_socket.sendto(b'Hello',server_address)
 
 image_data = {}
 
-
 while True:
 
     print("receiving...")
 
-
     packet = client_socket.recvfrom(PACK_SIZE)[0]
-    code, id = unpack(">BH", packet[:24])
+    code, id = unpack(">BH", packet[:3])
     data = packet[24:]
+
+    print(id)
+
+    print("data recieved")
 
     if code == Code.CONNECTION_END:
         exit()
@@ -42,20 +44,39 @@ while True:
 
     image_data[id] = data
 
-    print(image_data)
-
     if code == Code.FRAME_END.value or code == Code.FRAME_SOLO.value:
         
-        print(image_data)
+        image_data = sorted(image_data.items())
 
-        image_bytes = b''.join([payload[1] for payload in sorted(image_data.items())])
+        if image_data[-1][0] + 1 > len(image_data):
+            print("Not enough data: must have lost a packet")
+        else:
+            print(len(image_data))
+            print([packet_data[0] for packet_data in image_data])
+            print("Sufficient packets to create image (?)")
+        
+
+        image_bytes = b''.join([payload[1] for payload in image_data])
+
+        print("here")
+        with open("./recieved.txt", 'w') as out:
+            out.write(str(image_bytes))
+            out.close()
+        print("here 2")
+
+        client_socket.sendto(b"done", server_address)
+
 
         image_bytes_obj = BytesIO(image_bytes)
 
-        encoded_image = np.load(image_bytes_obj, allow_pickle=True)
+        print(image_bytes)
+
+        encoded_image = np.load(image_bytes_obj, allow_pickle=False)
 
         image = cv2.imdecode(encoded_image, 1)
         cv2.imshow("RECEIVING VIDEO", image)
+
+        exit()
     
     key = cv2.waitKey(10) & 0xFF
     if key == ord('q'):
